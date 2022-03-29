9f60416cc2b4c80fbba9be853fc1f36aa8b9a08f
lcr <746318202@qq.com>
2022-03-29 22:47:35 +0800

    初始化提交

---
+++ src/main/java/org/example/archetype/StartMainApplication.java
@@ -0,0 +1,11 @@
+package org.example.archetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartMainApplication.class, args);
+    }
+}
