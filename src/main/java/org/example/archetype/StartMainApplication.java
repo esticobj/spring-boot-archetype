8433664597228bd99963ed6701f4cac668c80603
lcr <746318202@qq.com>
2022-03-31 22:50:42 +0800

    增加mybatis支持

--- src/main/java/org/example/archetype/StartMainApplication.java
+++ src/main/java/org/example/archetype/StartMainApplication.java
@@ -1,9 +1,11 @@
 package org.example.archetype;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.example.archetype.dao")
 public class StartMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartMainApplication.class, args);
