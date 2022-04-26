cd85053575fc1cf013e6d5df915da34d70cb42ec
lcr <746318202@qq.com>
2022-04-26 20:27:24 +0800

    增加validation校验

--- src/main/java/org/example/archetype/common/handler/ControllerExceptionHandler.java
+++ src/main/java/org/example/archetype/common/handler/ControllerExceptionHandler.java
@@ -6,8 +6,12 @@ import org.example.archetype.common.exception.CommonErrorCode;
 import org.example.archetype.common.exception.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
@@ -16,13 +20,25 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler
     public BaseResponse<String> handleException(Exception e){
-        logger.error("controller抛出了异常", e);
+        logger.error("系统异常", e);
         return BaseResponseUtil.fail(CommonErrorCode.UNKHOWN_ERROR.getCode(), "系统异常");
     }
 
     @ExceptionHandler
     public BaseResponse<String> handleServiceException(ServiceException e){
-        logger.error("controller抛出了异常", e);
+        logger.error("业务异常", e);
         return BaseResponseUtil.fail(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler
+    public BaseResponse<String> handleConstraintViolationException(ConstraintViolationException e){
+        return BaseResponseUtil.fail(CommonErrorCode.ERROR_PARAM_FORMAT.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler
+    public BaseResponse<String> handleConstraintViolationException(BindException e){
+        String errorMsg = e.getBindingResult().getAllErrors().stream().map((cv) -> cv == null ? "null" : cv.getObjectName() + ": " + cv.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return BaseResponseUtil.fail(CommonErrorCode.ERROR_PARAM_FORMAT.getCode(), errorMsg);
     }
 }
